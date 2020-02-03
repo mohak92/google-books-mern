b92d6fd65ee7e03d54ea5e8ccbfe8af8aee115df
mohak92 <tamhane.mohak@gmail.com>
2020-02-02 21:56:36 -0500

    Header footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="page-footer">
+            <div className="container-fluid text-center">
+                <p className="footer">Mohak Tamhane &copy; 2020</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
