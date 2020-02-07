b94ca22d83b1a58b36203173c6804ada7c3e2653
mohak92 <tamhane.mohak@gmail.com>
2020-02-06 19:38:25 -0500

    routes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import SearchBooks from "./hoc/SearchBooks";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './Resources/css/styles.css'
@@ -12,9 +10,6 @@ function App() {
         <Router>
             <div>
                 <Header />
-                <Switch>
-                    <Route exact path="/" component={SearchBooks} />
-                </Switch>
                 <br />
                 <Footer />
             </div>
