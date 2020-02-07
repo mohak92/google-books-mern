0e231ea3167f8afa207ec9cbfbbf6a61da695f2a
mohak92 <tamhane.mohak@gmail.com>
2020-02-06 19:49:27 -0500

    routes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SearchBooks from './hoc/SearchBooks';
+import SearchBooks from './pages/SearchBooks';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './Resources/css/styles.css'
