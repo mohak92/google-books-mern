9502a615e3e2aa5501917e3cb74783fe723e52e1
mohak92 <tamhane.mohak@gmail.com>
2020-02-06 19:53:57 -0500

    new routes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchBooks from './pages/SearchBooks';
+import SaveBooks from './pages/SaveBooks';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './Resources/css/styles.css'
@@ -13,6 +14,7 @@ function App() {
                 <Header />
                 <Switch>
                 <Route exact path="/" component={SearchBooks}/>
+                <Route exact path="/saved" component={SaveBooks} />
                 </Switch>
                 <br />
                 <Footer />
